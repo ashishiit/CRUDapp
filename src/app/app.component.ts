7f62c62bcda754109e33433b10753a0b814849f6
Jamuda <ashish.turbobit@gmail.com>
2019-04-22 11:12:35 -0500

    djangoAngular

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { getComponentViewByInstance } from '@angular/core/src/render3/context_discovery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
+})
+export class AppComponent {
+  movies = [ {title: 'test movie'}];
+  constructor(private api: ApiService){
+      this.getMovies();
+  }
+  getMovies() = () =>{
+      this.api.getAllMovies().this.subscribe()
+      data => {
+          
+      }
+  }
+}
